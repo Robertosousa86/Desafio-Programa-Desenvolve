76158a80c78ca8a1de5dbd4b0480632a0fd70322
Roberto Sousa <robertosousa186@gmail.com>
2022-03-25 00:28:20 -0300

    Updating index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,13 @@
+const sendForm = document.querySelector('#main__btn');
+
 const validation = () => {
   const name = document.getElementById('name');
   const email = document.getElementById('email');
   const textArea = document.getElementById('text-area');
+
+  const regex = /^[A-Za-z0-9.]{1,32}@[A-Za-z0-9.-]{1,16}$/;
+
+  const emailTest = regex.test(email.value);
 
   if (name.value === '') {
     alert('Por favor digite seu Nome');
@@ -15,22 +21,21 @@ const validation = () => {
     return email.focus();
   }
 
-  if (textArea.value === '') {
-    alert('Erro no envio: Insira uma mensagem');
-
-    return textArea.focus();
-  }
-
-  const regex = /^[A-Za-z0-9.]{1,32}@[A-Za-z0-9.-]{1,16}$/;
-
-  const emailTest = regex.test(email.value);
-
   if (!emailTest) {
     alert('Erro no envio: Endereço de email inválido');
 
     return email.focus();
   }
 
+  if (textArea.value === '') {
+    alert('Erro no envio: Insira uma mensagem');
+
+    return textArea.focus();
+  }
+
   const user = name.value.split('@');
+
   return alert(`Obrigado pelo contato, ${user}! 😍`);
 };
+
+sendForm.addEventListener('click', () => validation());
